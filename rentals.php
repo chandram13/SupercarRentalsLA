2a7f11487ff21ccc36288666eaff2189e391461d
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-11 08:35:08 -0800

    Add files via upload

---
+++ rentals.php
@@ -0,0 +1,37 @@
+// Marvish Chandra
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Supercar Rentals Los Angeles</h1>
+
+<?php
+$lamborghiniaventadorS = 1990;
+echo $lamborghiniaventadorS;
+$ferrarif8spider = 1895;
+echo $ferrarif8spider;
+$mclaren720SSpider2022 = 1795;
+echo $mclaren720SSpider2022;
+$ferrarif8tributo = 1750;
+echo $ferrarif8tributo
+$ferrari488spider = 1595;
+echo $ferrari488spider;
+$ferrari488spiderred = 1595;
+echo $ferrari488spiderred;
+$ferrariportofino = 1495;
+echo $ferrariportofino;
+$lamborghinihuracanevospyder = 1490;
+echo $lamborghinihuracanevospyder;
+$lamborghiniurusmatteblack2021 = 1490;
+echo $lamborghiniurusmatteblack2021;
+$mclaren600LTspider = 1490;
+echo $mclaren600LTspider;
+$lamborghinihuracanevo2020 = 1350;
+echo $lamborghinihuracanevo2020;
+$lamborghiniurusyellow = 1190;
+echo $lamborghiniurusyellow;
+$lamborghiniurus2020 = 1190;
+echo $lamborghiniurus2020;
+?>
